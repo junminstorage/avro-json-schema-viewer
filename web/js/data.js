352a6ebcbdb7e342bfaad9e6b421581f36094c29
junminstorage <junmin.storage@gmail.com>
2015-12-18 21:09:02 -0500

    init check in

---
+++ web/js/data.js
@@ -0,0 +1,49 @@
+ var bbds_schema_list = [
+    {
+        "id": "1",
+        "value": "com.bloomberg.msg.BbdsEvents",
+        "label": "com.bloomberg.msg.BbdsEvents"
+    },
+    {
+        "id": "17",
+        "value": "com.bloomberg.msg.BIBitPub",
+        "label": "com.bloomberg.msg.BIBitPub"
+    },
+    {
+        "id": "18",
+        "value": "com.bloomberg.msg.EHUBNotification",
+        "label": "com.bloomberg.msg.EHUBNotification"
+    },
+    {
+        "id": "20",
+        "value": "com.bloomberg.msg.GlobalDataEmail",
+        "label": "com.bloomberg.msg.GlobalDataEmail"
+    },
+    {
+        "id": "22",
+        "value": "com.bloomberg.ds.DividendForecastEtf",
+        "label": "com.bloomberg.ds.DividendForecastEtf"
+    },
+    {
+        "id": "10",
+        "value": "com.bloomberg.ds.DtPipelineQC",
+        "label": "com.bloomberg.ds.DtPipelineQC"
+    },
+    {
+        "id": "11",
+        "value": "com.bloomberg.ds.BlsUnemploymentCatalog",
+        "label": "com.bloomberg.ds.BlsUnemploymentCatalog"
+    },
+    {
+        "id": "23",
+        "value": "com.bloomberg.ds.FixedIncomeCoverPool",
+        "label": "com.bloomberg.ds.FixedIncomeCoverPool"
+    },
+    {
+        "id": "24",
+        "value": "com.bloomberg.lib.Bbds",
+        "label": "com.bloomberg.lib.Bbds"
+    }
+   
+];
+    
