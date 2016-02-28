156647f1e04bf050b0cc95b383bbf9a15a086853
junminstorage <junmin.storage@gmail.com>
2016-02-28 10:37:07 -0500

    checked code to deal with enum

--- web/js/data.js
+++ web/js/data.js
@@ -1,49 +1,16 @@
  var bbds_schema_list = [
-    {
-        "id": "1",
-        "value": "com.bloomberg.msg.BbdsEvents",
-        "label": "com.bloomberg.msg.BbdsEvents"
-    },
-    {
-        "id": "17",
-        "value": "com.bloomberg.msg.BIBitPub",
-        "label": "com.bloomberg.msg.BIBitPub"
-    },
-    {
-        "id": "18",
-        "value": "com.bloomberg.msg.EHUBNotification",
-        "label": "com.bloomberg.msg.EHUBNotification"
-    },
-    {
-        "id": "20",
-        "value": "com.bloomberg.msg.GlobalDataEmail",
-        "label": "com.bloomberg.msg.GlobalDataEmail"
-    },
-    {
-        "id": "22",
-        "value": "com.bloomberg.ds.DividendForecastEtf",
-        "label": "com.bloomberg.ds.DividendForecastEtf"
-    },
-    {
-        "id": "10",
-        "value": "com.bloomberg.ds.DtPipelineQC",
-        "label": "com.bloomberg.ds.DtPipelineQC"
-    },
-    {
-        "id": "11",
-        "value": "com.bloomberg.ds.BlsUnemploymentCatalog",
-        "label": "com.bloomberg.ds.BlsUnemploymentCatalog"
-    },
-    {
-        "id": "23",
-        "value": "com.bloomberg.ds.FixedIncomeCoverPool",
-        "label": "com.bloomberg.ds.FixedIncomeCoverPool"
-    },
-    {
-        "id": "24",
-        "value": "com.bloomberg.lib.Bbds",
-        "label": "com.bloomberg.lib.Bbds"
-    }
-   
-];
+                 	    {
+                 	        "id": "1",
+                 	        "value": "com.bloomberg.ds.OpenFigiFund",
+                 	        "label": "com.bloomberg.ds.OpenFigiFund"
+                 	    	},
+                 	    	{"id": "2",
+                 	        "value": "com.bloomberg.ds.OpenFigiSwaps",
+                 	        "label": "com.bloomberg.ds.OpenFigiSwaps"
+                 	    	},
+                 	    	{"id": "3",
+                     	        "value": "com.bloomberg.ds.Extr",
+                     	        "label": "com.bloomberg.ds.Extr"
+                     	    	}
+                 	   		 ];
     
