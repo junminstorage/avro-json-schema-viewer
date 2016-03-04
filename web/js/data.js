8fe7538fdd2bcda001f69478ec3b9e81c6490666
junminstorage <junmin.storage@gmail.com>
2016-03-03 20:06:58 -0500

    add some dummy data

--- web/js/data.js
+++ web/js/data.js
@@ -1,16 +1,16 @@
  var bbds_schema_list = [
                  	    {
                  	        "id": "1",
-                 	        "value": "com.bloomberg.ds.OpenFigiFund",
-                 	        "label": "com.bloomberg.ds.OpenFigiFund"
+                 	        "value": "com.xyz.ds.fun",
+                 	        "label": "com.xyz.ds.fun"
                  	    	},
                  	    	{"id": "2",
-                 	        "value": "com.bloomberg.ds.OpenFigiSwaps",
-                 	        "label": "com.bloomberg.ds.OpenFigiSwaps"
+                 	        "value": "com.xyz.ds.swan",
+                 	        "label": "com.xyz.ds.swan"
                  	    	},
                  	    	{"id": "3",
-                     	        "value": "com.bloomberg.ds.Extr",
-                     	        "label": "com.bloomberg.ds.Extr"
+                     	        "value": "com.xyz.ds.Extr",
+                     	        "label": "com.xyz.ds.Extr"
                      	    	}
                  	   		 ];
     
